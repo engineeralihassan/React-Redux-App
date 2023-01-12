ac20597e037ef8f3d4df358f4c289ba714d67d69
Ali Hassan <engineralihassan@gmail.com>
2023-01-11 18:25:42 -0800

    Updates

---
+++ precache-manifest.f406fa8860fa63bddd6643db76f16264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc7560febcd3322da4458e09262348e8",
+    "url": "/React-Redux-App/index.html"
+  },
+  {
+    "revision": "a5722e04ca7d1270f40c",
+    "url": "/React-Redux-App/static/css/main.67d641af.chunk.css"
+  },
+  {
+    "revision": "6bbb2575db765f6d65f2",
+    "url": "/React-Redux-App/static/js/2.9d0b99b7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/React-Redux-App/static/js/2.9d0b99b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5722e04ca7d1270f40c",
+    "url": "/React-Redux-App/static/js/main.50310f91.chunk.js"
+  },
+  {
+    "revision": "f10db3c554a9dc873da6",
+    "url": "/React-Redux-App/static/js/runtime-main.f81fa1ef.js"
+  }
+]);
